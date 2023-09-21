a56e9999956a2d771ebc0c1a4fb654367a302325
anjali <anjalirajapakshaar@gmail.com>
2023-09-21 13:04:36 +0530

    configurations update

--- src/main/java/lk/ijse/gdse/assignment2springaad/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/gdse/assignment2springaad/config/WebAppRootConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse.assignment2springaad.config;
 
+import lk.ijse.gdse.assignment2springaad.service.ProjectServiceImpl;
+import lk.ijse.gdse.assignment2springaad.service.TechLeadServiceImpl;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan
 @EnableWebMvc
-@Import({JPAConfiguration.class})
+@Import({JPAConfiguration.class, TechLeadServiceImpl.class, ProjectServiceImpl.class})
 public class WebAppRootConfig {
 }
